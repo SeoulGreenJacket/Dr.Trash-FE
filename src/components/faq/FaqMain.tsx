7229f00a0a75a7cca25b32393fa19d33892fc79d
CWCTBOY <qkrdlswo98@gmail.com>
2022-10-30 16:05:00 +0900

    feat: faq

--- src/components/faq/FaqMain.tsx
+++
@@ -1,9 +0,0 @@
-let faq1 = require('../../../assets/faq/pet_label.jpeg');
-let faq2 = require('../../../assets/faq/food_waste.jpeg');
-let faq3 = require('../../../assets/faq/plastic_bag.jpeg');
-
-const FaqMain = () => {
-  return null;
-};
-
-export default FaqMain;
